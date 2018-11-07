9ecbcc82877cdb0875a1cd122d0c29dde6a89c92
Yakoot <mail@tbrd.ru>
2018-11-08 01:05:33 +0300

    sdf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="filter">
+    <div class="first">
+      <Multiselect :showLabels="false" v-model="filter.first" track-by="value" label="label" placeholder="Select one" :options="optionsFirst" :searchable="false" :allow-empty="false" :option-height="61">
+        <template slot="singleLabel" slot-scope="{ option }"><strong>{{ option.label }}</strong></template>
+        <template slot="caret">
+          <span class="caret">
+            <font-awesome-icon icon="angle-down" />
+          </span>
+        </template>
+      </Multiselect>
+    </div>
+    <div class="second">
+      <v-select :searchable="false" placeholder="select" :options="optionsFirst"></v-select>
+    </div>
+    <div class="third">
+      <v-select :searchable="false" placeholder="select" :options="optionsFirst"></v-select>
+    </div>
   </div>
 </template>
 
 <script>
+import Multiselect from "vue-multiselect";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: { Multiselect },
+  data() {
+    return {
+      optionsFirst: [
+        { label: "Sort by: recommended", value: 0 },
+        { label: "Sort by: low to high", value: 1 },
+        { label: "Sort by: high to low", value: 2 }
+      ],
+      filter: {
+        first: { label: "Sort by: recommended", value: 0 }
+      }
+    };
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+.filter {
+  display: grid;
+  grid-template-areas: "first second third";
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.first {
+  grid-area: first;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.second {
+  grid-area: second;
 }
-a {
-  color: #42b983;
+.third {
+  grid-area: third;
+}
+
+.caret {
+    position: absolute;
+    right: 10px;
+    font-size: 25px;
+    top: 7px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import BootstrapVue from 'bootstrap-vue'
+import vSelect from 'vue-select'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faAngleDown)
+
+// Vue.use(BootstrapVue)
+Vue.component('v-select', vSelect)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
